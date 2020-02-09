2c841f76174db3c6395e56680dc6723580958dec
Oleksandr Melnyk <alek.melnyk@gmail.com>
2020-02-09 22:51:08 +0200

    fixed menu opening
    now closes on click outside of menu, does not close on clicking inside

--- dist/js/menu_opener.js
+++ dist/js/menu_opener.js
@@ -1,15 +1,24 @@
+const menu = document.querySelector('.mob-menu');
+menu.addEventListener('click', event => event.stopPropagation());
+
 const burger = document.querySelector('.mob-header__burger');
+burger.addEventListener('click', toggleMenu);
 
-burger.addEventListener('click', moveMenu);
-
-function moveMenu () {
-    const menu = document.querySelector('.mob-menu');
-    
+function toggleMenu (event) {
+    event.stopPropagation();
     if (menu.style.right === '' || menu.style.right === '-400px') {
         menu.style.display = 'block';
-        setTimeout(() => menu.style.right = 0, 0)
+        setTimeout(() => menu.style.right = 0, 0);
     } else {
         menu.style.right = '-400px';
-        setTimeout(() => menu.style.display = 'none', 500)
+        setTimeout(() => menu.style.display = 'none', 500);
     }
+}
+
+const body = document.querySelector('body');
+body.addEventListener('click', closeMenu);
+
+function closeMenu () {
+    menu.style.right = '-400px';
+    setTimeout(() => menu.style.display = 'none', 500);
 }
